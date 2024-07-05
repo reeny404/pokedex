e8c2f29e1887a9e74163592a81133357386c2ef5
reeny404 <happy2hyeon@kakao.com>
2024-07-05 20:31:44 +0900

    feat: MessageBox Enter로 다음 메세지로 넘길 수 있도록

--- src/components/MessageBox.tsx
+++ src/components/MessageBox.tsx
@@ -1,10 +1,25 @@
-import { PropsWithChildren } from "react";
+"use client";
+
+import { PropsWithChildren, useEffect } from "react";
 
 type MessageBoxProps = {
   onClick?: () => void;
 } & PropsWithChildren;
 
 function MessageBox({ children, onClick }: MessageBoxProps) {
+  useEffect(() => {
+    if (!onClick) {
+      return;
+    }
+    const handleKeybaordEnter = (e: KeyboardEvent) => {
+      if (e.key === "Enter") onClick();
+    };
+    window.document.addEventListener("keydown", handleKeybaordEnter);
+
+    return () =>
+      window.document.removeEventListener("keydown", handleKeybaordEnter);
+  }, [onClick]);
+
   return (
     <div
       className="w-full min-h-44 py-8 px-12 rounded border-double border-4 border-gray-900 font-semibold text-4xl"
